1f4ea961d6e220e46aa5e327e19fa4df9ae24de1
Soumyadip Moni <116944847+AvaterClasher@users.noreply.github.com>
2024-06-22 23:03:06 +0530

    Trying eslint error

--- next.config.js
+++ next.config.js
@@ -6,5 +6,12 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {};
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 
 export default config;
